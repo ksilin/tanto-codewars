6ff58f06f0c811b72d4ddc51e4d343ec5ef0ad73
ksilin <konstantin.silin@gmail.com>
2013-11-19 15:59:23 +0100

    added age_sex_check spec: http://www.codewars.com/dojo/katas/5143b3d15c6bf6448d00005d/discuss#528b26bb123f1f25f00002dchttp://www.codewars.com/dojo/katas/5143b3d15c6bf6448d00005d/solutions/ruby

---
+++ specs/k6_age_sex_check.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+
+def check_ages?(data, sex, age_is_older_than)
+  data.reduce([]){|res, e|
+    res << e if sex == e[:sex]
+    res
+  }.all?{|e|
+    e[:age] > age_is_older_than
+  }
+end
+
+def check_ages2?(data, sex, age_is_older_than)
+  data.reject{| e|
+     sex != e[:sex]
+  }.all?{|e|
+    age_is_older_than < e[:age]
+  }
+end
+
+data = [
+    {age: 40, sex: :m},
+    {age: 24, sex: :f},
+    {age: 56, sex: :m},
+    {age: 45, sex: :m},
+    {age: 23, sex: :f}
+]
+
+describe 'filters array of hashes by sex' do
+
+  p check_ages2?(data, :f, 22)
+
+end
